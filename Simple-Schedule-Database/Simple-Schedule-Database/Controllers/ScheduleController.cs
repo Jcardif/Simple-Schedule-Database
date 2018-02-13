c1210db4b688fe902c1509941b89f77c968410b7
Josh Cardif <josh.cardif14@outlook.com>
2018-02-13 08:02:12 -0800

    changed schedule conntroler file

--- Simple-Schedule-Database/Simple-Schedule-Database/Controllers/ScheduleController.cs
+++ Simple-Schedule-Database/Simple-Schedule-Database/Controllers/ScheduleController.cs
@@ -8,36 +8,36 @@ using Simple_Schedule_Database.Models;
 
 namespace Simple_Schedule_Database.Controllers
 {
-    public class SheduleController : ApiController
+    public class ScheduleController : ApiController
     {
-        // GET: api/Shedule
+        // GET: api/Schedule
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Shedule/5
+        // GET: api/Schedule/5
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/Shedule
+        // POST: api/Schedule
         public HttpResponseMessage Post([FromBody]Schedule schedule)
         {
-            SchedulePersistence sp=new SchedulePersistence();
+            SchedulePersistence sp = new SchedulePersistence();
             int id = sp.CreateNewSchedule(schedule);
-            HttpResponseMessage response=new HttpResponseMessage(HttpStatusCode.Created);
-            response.Headers.Location=new Uri(Request.RequestUri, string.Format($"Schedule/{id}"));
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Created);
+            response.Headers.Location = new Uri(Request.RequestUri, string.Format($"Schedule/{id}"));
             return response;
         }
 
-        // PUT: api/Shedule/5
+        // PUT: api/Schedule/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/Shedule/5
+        // DELETE: api/Schedule/5
         public void Delete(int id)
         {
         }
