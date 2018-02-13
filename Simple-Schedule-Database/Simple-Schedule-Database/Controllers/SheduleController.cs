60e46c30f2f1fc23bdc40809e6fab1c854edcb38
Josh Cardif <josh.cardif14@outlook.com>
2018-02-13 08:00:29 -0800

    Create a new schedule

---
+++ Simple-Schedule-Database/Simple-Schedule-Database/Controllers/SheduleController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Simple_Schedule_Database.Models;
+
+namespace Simple_Schedule_Database.Controllers
+{
+    public class SheduleController : ApiController
+    {
+        // GET: api/Shedule
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Shedule/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Shedule
+        public HttpResponseMessage Post([FromBody]Schedule schedule)
+        {
+            SchedulePersistence sp=new SchedulePersistence();
+            int id = sp.CreateNewSchedule(schedule);
+            HttpResponseMessage response=new HttpResponseMessage(HttpStatusCode.Created);
+            response.Headers.Location=new Uri(Request.RequestUri, string.Format($"Schedule/{id}"));
+            return response;
+        }
+
+        // PUT: api/Shedule/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Shedule/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Simple-Schedule-Database/Simple-Schedule-Database/SchedulePersistence.cs
+++ Simple-Schedule-Database/Simple-Schedule-Database/SchedulePersistence.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
+using MySql.Data.MySqlClient;
+using Simple_Schedule_Database.Models;
 
 namespace Simple_Schedule_Database
 {
     public class SchedulePersistence
     {
+        public int CreateNewSchedule(Schedule schedule)
+        {
+            MySqlConnection conn;
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["scheduleDbConnectionString"].ConnectionString;
+            conn=new MySqlConnection();
+            try
+            {
+                conn.Open();
+                conn.ConnectionString = connectionString;
+
+                string sqlString =
+                    $"INSERT INTO scheduletbl (Date, Activity, Locality) VALUES ({schedule.Date:D}, {schedule.Activity}, {schedule.Locality})";
+                MySqlCommand cmd = new MySqlCommand(sqlString, conn);
+                cmd.ExecuteNonQuery();
+                int id = Convert.ToInt32(cmd.LastInsertedId);
+                return id;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
